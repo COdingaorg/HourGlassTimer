1fd70c41bf109ed4d0454a5d06c2952bd826d994
Caleb Odinga <calemasanga@gmail.com>
2021-05-05 21:49:57 +0300

    adds seting time using current date

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
 //set time we are counting to
-$(document).ready(function(){
-  var time = 1;
-  var perc = time+'%';
-    $('#upperGlass div').css('height',perc);
+$(document).ready(function () {
+  var deviceDate = new Date().toLocaleDateString();
+  var deviceTime = new Date().toLocaleTimeString();
+  var dateArray = deviceDate.split('/').reverse().join(' ');
+
+  $('input#timeInput').submit(function (event) {
+    event.preventDefault();
+    console.log('Before')
+    var timerHours = $('#hours').val();
+    var timerMinutes = $('input#minutes').val();
+    var timerSeconds = $('input#seconds').val();
+    var timerSet = timerHours + ' ' + timerMinutes + ' ' + timerSeconds;
+    var userSet = new Date(dateArray);
+    console.log('timerHours');
+
+  })
+  // var userDate = new Date()
+  // var userTime = $('input#hours').val();
+  // var time = 87;
+  // var percAdd = time+'%';
+  // var percSub = (100-time)+'%';
+  //   $('#upperGlass div').css('height',percAdd);
+  //   $('#lowerGlass div').css('height',percSub);
 })
