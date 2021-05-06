959576bde006a6ca9a6415be0434af63a8d17cac
Caleb Odinga <calemasanga@gmail.com>
2021-05-06 11:04:24 +0300

    get difference between users set time and device time, converts them to days hours minutes and seconds

--- scripts.js
+++ scripts.js
@@ -2,24 +2,31 @@
 $(document).ready(function () {
   $('button#submit').click(function (event) {
     event.preventDefault();
+      //setting user time to count down from
+      //getting input from form
+    var timerHours = parseInt($('input#hours').val());
+    var timerMinutes = parseInt($('input#minutes').val());
+    var timerSeconds = parseInt($('input#seconds').val());
 
-    var timerHours = $('input#hours').val();
-    var timerMinutes = $('input#minutes').val();
-    var timerSeconds = $('input#seconds').val();
-    var userSet = `${timerHours}:${timerMinutes}:${timerSeconds}`
-    var userDate = `${new Date().getFullYear()} ${new Date().getMonth()} ${new Date().getDate()}`;
-    var deviceTIme = new Date().getTime();
-    var userTime = 
+    //device time
+    var deviceTime = new Date().getTime();
+    var userSet = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), timerHours, timerMinutes, timerSeconds);
 
-    console.log(userDate);
+    //user Time
+    var userTime = (userSet.getTime())
+   
+    //getting the difference
+    var distance = userTime-deviceTime;
 
+   // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
 
-     // Time calculations for days, hours, minutes and seconds
-  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  })
+  console.log(days, hours, minutes, seconds);
+    console.log();
   // var userDate = new Date()
   // var userTime = $('input#hours').val();
   // var time = 87;
@@ -27,4 +34,5 @@ $(document).ready(function () {
   // var percSub = (100-time)+'%';
   //   $('#upperGlass div').css('height',percAdd);
   //   $('#lowerGlass div').css('height',percSub);
+})
 })
